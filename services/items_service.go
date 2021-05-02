3017f9d8eb75b6584070a01eea294704e29a9f11
Sinthorn <psinthorn@gmail.com>
2021-05-02 18:13:38 +0700

    1st initial commit

--- services/items_service.go
+++ services/items_service.go
@@ -10,6 +10,7 @@ type itemsService struct{}
 type itemsServiceInterface interface {
 	Create(items.Item) (*items.Item, error)
 	Get(items.Item) (*items.Item, error)
+	GetByID(int) *items.Item
 }
 
 func (i *itemsService) Create(item items.Items) (*items.Item, error) {}
